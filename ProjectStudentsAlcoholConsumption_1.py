dc3cea1abf21eceb3a9ef57b10a62221c19dd775
Shapito229 <89279119+Shapito229@users.noreply.github.com>
2022-12-14 12:13:45 +0300

    Add files via upload

--- ProjectStudentsAlcoholConsumption_1.py
+++ ProjectStudentsAlcoholConsumption_1.py
@@ -292,9 +292,9 @@ st.write(a1)
 data_corr = pd.DataFrame(data=alcohol, columns=['Alc', 'failures', 'absences', 'age', 'health', 'sex', 'goout'])
 #display(data_corr.corr())
 st.write(data_corr.corr())
-#pd.plotting.scatter_matrix(data_corr, figsize=(20, 10))
+fig1 = pd.plotting.scatter_matrix(data_corr, figsize=(20, 10))
 #plt.show()
-st.plotly_chart(pd.plotting.scatter_matrix(data_corr, figsize=(20, 10)))
+st.vega_lite_chart(fig1)
 
 # Hooray! There is a correlation and it is quite strong, let's visualize it!
 
